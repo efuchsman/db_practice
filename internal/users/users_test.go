082f8b3b48bf567cb377235ba872ff44e82a01db
Eli Fuchsman <efuchsman@gmail.com>
2024-04-11 15:45:38 -0600

    Add more testing

--- internal/users/users_test.go
+++ internal/users/users_test.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"database/sql"
 	"db_practice/internal/db"
 	"fmt"
 	"os"
@@ -75,7 +76,7 @@ func TestCreateUser(t *testing.T) {
 		description    string
 		db             *db.TestClient
 		expectedOutput *User
-		expectedErr    bool
+		expectedErr    error
 	}{
 		{
 			description: "Success: User created",
@@ -93,14 +94,14 @@ func TestCreateUser(t *testing.T) {
 				ZipCode:     "80108",
 				DateOfBirth: "12/14/1993",
 			},
-			expectedErr: false,
+			expectedErr: nil,
 		},
 		{
 			description: "Failure: Email already exists",
 			db: &db.TestClient{
 				CreateUserErr: db.ErrEmailExists,
 			},
-			expectedErr: true,
+			expectedErr: db.ErrEmailExists,
 		},
 	}
 	for i, tc := range testCases {
@@ -110,8 +111,8 @@ func TestCreateUser(t *testing.T) {
 
 			c := NewUsersClient(tc.db)
 			user, err := c.CreateUser(testUserEli.FirstName, testUserEli.LastName, testUserEli.Email, testUserEli.Address, testUserEli.City, testUserEli.State, testUserEli.ZipCode, testUserEli.DateOfBirth)
-			if tc.expectedErr != false {
-				assert.Equal(t, tc.expectedErr, true)
+			if tc.expectedErr != nil {
+				assert.Equal(t, tc.expectedErr.Error(), err.Error())
 			} else {
 				assert.NotNil(t, user)
 				assert.NotNil(t, user.Id)
@@ -119,6 +120,68 @@ func TestCreateUser(t *testing.T) {
 				require.NoError(t, err)
 				assert.Equal(t, tc.expectedOutput.FirstName, user.FirstName)
 				assert.Equal(t, tc.expectedOutput.LastName, user.LastName)
+			}
+		})
+	}
+}
+
+func TestGetUserById(t *testing.T) {
+	testCases := []struct {
+		description    string
+		id             string
+		db             *db.TestClient
+		expectedOutput *User
+		expectedErr    error
+	}{
+		{
+			description: "Success: User found",
+			id:          testUserEli.Id,
+			db: &db.TestClient{
+				CreateUserData:  testUserEli,
+				GetUserByIdData: testUserEli,
+			},
+			expectedOutput: &User{
+				Id:          "12infioed",
+				FirstName:   "Eli",
+				LastName:    "Fuchsman",
+				Email:       "testEmail@mail.com",
+				Address:     "1123 Street St.",
+				City:        "Denver",
+				State:       "CO",
+				ZipCode:     "80108",
+				DateOfBirth: "12/14/1993",
+			},
+			expectedErr: nil,
+		},
+		{
+			description: "Failure: No user found",
+			id:          testUserEli.Id,
+			db: &db.TestClient{
+				CreateUserData: testUserEli,
+				GetUserByIdErr: sql.ErrNoRows,
+			},
+			expectedOutput: nil,
+
+			expectedErr: sql.ErrNoRows,
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+			t.Log(tc.description)
+
+			c := NewUsersClient(tc.db)
+
+			_, err := c.CreateUser(testUserEli.FirstName, testUserEli.LastName, testUserEli.Email, testUserEli.Address, testUserEli.City, testUserEli.State, testUserEli.ZipCode, testUserEli.DateOfBirth)
+			require.NoError(t, err)
+
+			user, err := c.GetUserById(tc.id)
+			if tc.expectedErr != nil {
+				assert.Equal(t, tc.expectedErr.Error(), err.Error())
+			} else {
+				assert.NotNil(t, user)
+				assert.NoError(t, err, tc.description)
+				assert.Equal(t, user, tc.expectedOutput)
 			}
 		})
 	}
